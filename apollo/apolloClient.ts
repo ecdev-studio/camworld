bd3435ed03a24a18609f6f0c0f9704508b1b4b51
Bohdan Oleksiuk <lordapos@gmail.com>
2022-04-04 16:07:51 +0300

    install apolloClient

---
+++ apollo/apolloClient.ts
@@ -0,0 +1,50 @@
+import {useMemo} from 'react'
+import {ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject} from '@apollo/client'
+import merge from 'deepmerge'
+import isEqual from 'lodash/isEqual'
+
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
+
+let apolloClient: ApolloClient<NormalizedCacheObject>
+
+function createApolloClient() {
+	return new ApolloClient({
+		ssrMode: typeof window === 'undefined',
+		link: new HttpLink({
+			uri: 'https://backend.ecdevstudio.com/graphql',
+		}),
+		cache: new InMemoryCache(),
+	})
+}
+
+export function initializeApollo(initialState = null) {
+	const _apolloClient = apolloClient ?? createApolloClient()
+	if (initialState) {
+		const existingCache = _apolloClient.extract()
+		const data = merge(existingCache, initialState, {
+			arrayMerge: (destinationArray, sourceArray) => [
+				...sourceArray,
+				...destinationArray.filter((d) =>
+					sourceArray.every((s) => !isEqual(d, s))
+				),
+			],
+		})
+		_apolloClient.cache.restore(data)
+	}
+	if (typeof window === 'undefined') return _apolloClient
+	if (!apolloClient) apolloClient = _apolloClient
+	return _apolloClient
+}
+
+export function addApolloState(client: { cache: { extract: () => any } }, pageProps: { props: { [x: string]: any } }) {
+	if (pageProps?.props) {
+		pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract()
+	}
+
+	return pageProps
+}
+
+export function useApollo(pageProps: { [x: string]: any }) {
+	const state = pageProps[APOLLO_STATE_PROP_NAME]
+	return useMemo(() => initializeApollo(state), [state])
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {store} from '../store'
 import {Provider} from 'react-redux'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../apollo/apolloClient'
 
 function MyApp({Component, pageProps}: AppProps) {
+	const apolloClient = useApollo(pageProps)
+
 	return (
 		<Provider store={store}>
-			<Component {...pageProps} />
+			<ApolloProvider client={apolloClient}>
+				<Component {...pageProps} />
+			</ApolloProvider>
 		</Provider>
 	)
 }
