110d72071733151d1d330f8daf61778f4f74b875
YuriiYevtushenko <mask0nw2@gmail.com>
2022-04-08 15:54:33 +0300

    fixed reducer

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -3,10 +3,24 @@ import {NextComponentType} from "next";
 import {IProduct} from "../../types/data-types";
 import Link from 'next/link'
 import Image from "next/image";
+import {changeQuantity} from "../../store/action-creator/global-action-creator";
+import {useTypedSelector} from "../../hook/useTypedSelector";
+import {store} from "../../store";
+
+
 const ProductCard: NextComponentType<{}, {}, { product: IProduct }> = ({product}) => {
   const addToCartHandler = () => {
-
+      const item = {
+          name: product.name,
+          image: product.image,
+          price: product.price,
+          id: product.id,
+          slug: product.slug,
+          quantity:1
+      }
+      store.dispatch(changeQuantity(item,1))
   }
+
   return (
     <>
       <Link href={product.slug}>
--- store/action-creator/global-action-creator.ts
+++ store/action-creator/global-action-creator.ts
@@ -1,14 +1,14 @@
 import {AppActionEnums} from '../redux-enum'
 import {ICart} from "../../types/redux-types";
 import {useTypedSelector} from "../../hook/useTypedSelector";
+import {store} from "../index";
 
 export const toggleMenu = (visible: boolean) => {
     return {type: AppActionEnums.MOBILE_NAVIGATION_MENU, payload: visible}
 }
 
-export const ChangeQuantity = (item: ICart, quantity: number) => {
-    let existItem = useTypedSelector(state => state.cart && state.cart.length > 0 && state.cart.find(x => x.id === item.id))
-
+export const changeQuantity = (item: ICart, quantity: number) => {
+    const existItem = store.getState().cart.find(x => x.id === item.id)
     if (existItem) {
         if (existItem.quantity + quantity > 0) quantity = existItem.quantity + quantity
         else quantity = 0
@@ -26,9 +26,9 @@ export const ChangeQuantity = (item: ICart, quantity: number) => {
     if (quantity === 0) return {type: AppActionEnums.CART_REMOVE_ITEM, payload: payload}
     else return {type: AppActionEnums.CART_CHANGE_QUANTITY, payload: payload}
 }
-export const RemoveCartItem = (item: ICart) => {
+export const removeCartItem = (item: ICart) => {
     return {type: AppActionEnums.CART_REMOVE_ITEM, payload: item}
 }
-export const ClearCart = () => {
+export const clearCart = () => {
     return {type: AppActionEnums.CART_CLEAR_ALL}
 }
