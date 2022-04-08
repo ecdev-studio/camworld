e56b2a2b41f84c4f277a4ebccd07c0fa31a77a07
YuriiYevtushenko <mask0nw2@gmail.com>
2022-04-08 18:52:33 +0300

    added redux cart subscribe

--- store/index.ts
+++ store/index.ts
@@ -6,3 +6,16 @@ import {rootReducer} from './reducers'
 export const store = createStore(rootReducer, composeWithDevTools(
     applyMiddleware(thunk)
 ))
+let currentCartValue = store.getState().cart
+store.subscribe(() => {
+    let previousCartValue = currentCartValue
+    currentCartValue = store.getState().cart
+    if (currentCartValue !== previousCartValue) {
+        try {
+            const serializedState = JSON.stringify(store.getState().cart);
+            localStorage.setItem('orders', serializedState);
+        } catch (e) {
+            console.log(e)
+        }
+    }
+})
