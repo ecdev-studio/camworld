201e1dc8b9fe8cb79f352695a46a21f7e163c820
Bohdan Oleksiuk <lordapos@gmail.com>
2022-04-26 20:54:33 +0300

    change routes

--- pages/[category].tsx
+++ pages/[category].tsx
@@ -59,8 +59,11 @@ const Category: NextPage<PageProps> = (
 
 	useEffect(() => {
 		setUpdate(true)
-	}, []);
 
+		return () => {
+			setUpdate(false)
+		}
+	}, []);
 
 	useEffect(() => {
 		if (dataSort && !dataLoading) {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,15 @@ import {store} from '../store'
 import {Provider} from 'react-redux'
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../apollo/apolloClient'
+import {useRouter} from "next/router";
 
 function MyApp({Component, pageProps}: AppProps) {
 	const apolloClient = useApollo(pageProps)
-
+	const router = useRouter();
+	const path = (/#!(\/.*)$/.exec(router.asPath) || [])[1];
+	if (path) {
+		router.replace(path);
+	}
 	return (
 		<Provider store={store}>
 			<ApolloProvider client={apolloClient}>
